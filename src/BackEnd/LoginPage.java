e9cf4e19f791fb227ba01d3a639f72b958c44242
Andrew <trinh.96.andrew@gmail.com>
2020-02-02 15:29:01 -0800

    Eclipse project

---
+++ src/BackEnd/LoginPage.java
@@ -0,0 +1,46 @@
+package BackEnd;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginPage
+ */
+@WebServlet("/LoginPage")
+public class LoginPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String html = "<!DOCTYPE html>\r\n" + 
+				"<html>\r\n" + 
+				"  <head>\r\n" + 
+				"    <title>CSCI 201 CSS</title>\r\n" + 
+				"  </head>" +
+				"</html>";
+		
+		response.getWriter().append(html);
+		
+		//just tryna commit
+	}
+
+
+
+}
